d8dfd539367a09bae26d269863fea09aebd4f77e
JONATHAN THOMAS URK21CS2036 <91773936+jonathanthomas19@users.noreply.github.com>
2022-08-03 20:41:13 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,29 +3,24 @@ class Stack:
     def __init__(self, size):
         self.items = []
         self.size = size
-
+    
     def is_empty(self):
-        # Write code here
         return self.top == -1
-
+    
     def is_full(self):
-        # Write code here
         return self.top == (self.size - 1)
-
+    
     def push(self, data):
         if not self.is_full():
-            # Write code here
             self.top +=1
             self.items[self.top] = data
-
+    
     def pop(self):
         if not self.is_empty():
-            # Write code here
             x = self.items[self.top]
             self.top +=1
 
     def status(self):
-        # Write code here
         for i in range(self.top + 1):
             print(self.items[i])
 
