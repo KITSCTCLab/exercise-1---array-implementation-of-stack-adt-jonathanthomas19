7d4a1b9dbbf7fc18e5a57175c9d55bcb65e69169
JONATHAN THOMAS URK21CS2036 <91773936+jonathanthomas19@users.noreply.github.com>
2022-08-09 15:22:15 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,36 +1,43 @@
 import os
-class Stack:
-    def __init__(self, size):
-        self.items = []
-        self.size = size
-    
-    def is_empty(self):
-        return self.top == -1
-    
-    def is_full(self):
-        return self.top == (self.size - 1)
-    
-    def push(self, data):
-        if not self.is_full():
-            self.top +=1
-            self.items[self.top] = data
-    
-    def pop(self):
-        if not self.is_empty():
-            x = self.items[self.top]
-            self.top +=1
+	class Stack:
+	    def __init__(self, size):
+	        self.items = []
+	        self.size = size
+	
 
-    def status(self):
-        for i in range(self.top + 1):
-            print(self.items[i])
+	    def is_empty(self):
+	        # Write code here
+	        return len(self.items) == 0
+	 
+	
 
-# Do not change the following code
-size, queries = map(int, input().rstrip().split())
-stack = Stack(size)
-for line in range(queries):
-    values = list(map(int, input().rstrip().split()))
-    if values[0] == 1:
-        stack.push(values[1])
-    elif values[0] == 2:
-        stack.pop()
-stack.status()
+	    def is_full(self):
+	        return len(self.items) >= self.size
+	
+
+	    def push(self, data):
+	        if not self.is_full():
+	            self.items.append(data)
+	
+
+	    def pop(self):
+	        if not self.is_empty():
+	            self.removed_element = self.items.pop()
+	
+
+	    def status(self):
+	        for numbers in self.items:
+	            print (numbers)
+	
+
+	# Do not change the following code
+	size, queries = map(int, input().rstrip().split())
+	stack = Stack(size)
+	for line in range(queries):
+	    values = list(map(int, input().rstrip().split()))
+	    if values[0] == 1:
+	        stack.push(values[1])
+	    elif values[0] == 2:
+	        stack.pop()
+	stack.status()
+
